0b6d5e9a8674e35807dea120c8053c5af8ef1c19
alexandergagliano <alexandertgagliano@gmail.com>
2025-04-26 11:22:21 -0400

    Update sample function

--- src/astro_prost/__init__.py
+++ src/astro_prost/__init__.py
@@ -1,4 +1,4 @@
 from .associate import associate_transient
 from .diagnose import diagnose_ranking, plot_match
 
-__all__ = ["associate_transient", "diagnose_ranking", "plot_match"]
+__all__ = ["associate_transient", "diagnose_ranking", "plot_match", "associate_sample"]
